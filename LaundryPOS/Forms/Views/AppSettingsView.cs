e9cd05d50163243072fe581bf116e5410b79d716
Jansen Cruz <103548207+jansencruz23@users.noreply.github.com>
2023-09-20 21:47:59 +0800

    Display AppSettings in receipt

--- LaundryPOS/Forms/Views/AppSettingsView.cs
+++ LaundryPOS/Forms/Views/AppSettingsView.cs
@@ -1,5 +1,7 @@
-﻿using LaundryPOS.Contracts;
+﻿using Guna.UI2.WinForms.Suite;
+using LaundryPOS.Contracts;
 using LaundryPOS.Models;
+using Microsoft.Reporting.Map.WebForms.BingMaps;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,6 +16,7 @@ namespace LaundryPOS.Forms.Views
 {
     public partial class AppSettingsView : UserControl
     {
+        private const int FIRST_VALUE = 1;
         private readonly IUnitOfWork _unitOfWork;
 
         public AppSettingsView(IUnitOfWork unitOfWork)
@@ -27,17 +30,18 @@ namespace LaundryPOS.Forms.Views
         {
             if (Enum.TryParse(cbTheme.SelectedValue!.ToString(), out Theme theme))
             {
-                var appSettings = new AppSettings
-                {
-                    Name = txtName.Text,
-                    Address = txtAddress.Text,
-                    Description = txtDescription.Text,
-                    PhoneNumber = txtNumber.Text,
-                    Email = txtEmail.Text,
-                    Theme = theme
-                };
+                var appSettings = await _unitOfWork.AppSettingsRepo.GetByID(FIRST_VALUE);
 
-                _unitOfWork.AppSettingsRepo.Insert(appSettings);
+                if (appSettings != null)
+                {
+                    UpdateAppSettings(appSettings, theme);
+                }
+                else
+                {
+                    appSettings = CreateNewAppSettings(theme);
+                    _unitOfWork.AppSettingsRepo.Insert(appSettings);
+                }
+                
                 await _unitOfWork.SaveAsync();
             }
             else
@@ -50,6 +54,26 @@ namespace LaundryPOS.Forms.Views
         {
             var themes = Enum.GetValues(typeof(Theme)) as Theme[];
             cbTheme.DataSource = themes;
+        }
+
+        private AppSettings CreateNewAppSettings(Theme theme) => new()
+        {
+            Name = txtName.Text,
+            Address = txtAddress.Text,
+            Description = txtDescription.Text,
+            PhoneNumber = txtNumber.Text,
+            Email = txtEmail.Text,
+            Theme = theme
+        };
+
+        private void UpdateAppSettings(AppSettings appSettings, Theme theme)
+        {
+            appSettings.Name = txtName.Text;
+            appSettings.Address = txtAddress.Text;
+            appSettings.Description = txtDescription.Text;
+            appSettings.PhoneNumber = txtNumber.Text;
+            appSettings.Email = txtEmail.Text;
+            appSettings.Theme = theme;
         }
     }
 }
